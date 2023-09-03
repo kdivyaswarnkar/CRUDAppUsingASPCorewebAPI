29fb7d6e947008a7035d1e9b6d49fcf151b5fff6
Divya Swarnkar <kdivyaswarnkar@gmail.com>
2023-09-03 08:34:44 +0530

    Add project files.

---
+++ CRUDAppUsingASPCorewebAPI/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using CRUDAppUsingASPCorewebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace CRUDAppUsingASPCorewebAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7102/api/StudentAPI/";
+        private HttpClient client=new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = new();
+            HttpResponseMessage response= client.GetAsync(url).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                string result=response.Content.ReadAsStringAsync().Result;
+                var data=JsonConvert.DeserializeObject<List<Student>>(result);
+                if(data !=null)
+                {
+                    students = data;
+                }
+            }
+            return View(students);
+        }
+    }
+}
---
+++ CRUDAppUsingASPCorewebAPI/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDAppUsingASPCorewebAPI.Models
+{
+        public class Student
+        {
+            public int id { get; set; }
+            public string studentName { get; set; }
+            public string studentGender { get; set; }
+            public int age { get; set; }
+            public int standard { get; set; }
+            public string fatherName { get; set; }
+        }
+}
