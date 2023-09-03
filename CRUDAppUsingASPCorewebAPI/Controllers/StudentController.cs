00b53160e28aa771ff147259fa037cdb613b4ab0
Divya Swarnkar <kdivyaswarnkar@gmail.com>
2023-09-03 08:56:46 +0530

    Added Create Student & Get Student to display

--- CRUDAppUsingASPCorewebAPI/Controllers/StudentController.cs
+++ CRUDAppUsingASPCorewebAPI/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using CRUDAppUsingASPCorewebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace CRUDAppUsingASPCorewebAPI.Controllers
@@ -25,6 +26,45 @@ namespace CRUDAppUsingASPCorewebAPI.Controllers
                 }
             }
             return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            //to do : convert std in to json
+            string data=JsonConvert.SerializeObject(std);
+            //to do : formated text we use stringcontent (stringcontent return content)
+            StringContent content=new StringContent(data,Encoding.UTF8,"application/json");
+            HttpResponseMessage response=client.PostAsync(url,content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["insert_message"] = "Student Added Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        { 
+            Student std= new Student();
+            HttpResponseMessage response = client.GetAsync(url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Student>(result);
+                if (data != null)
+                {
+                    std = data;
+                }
+            }
+            return View(std);
         }
     }
 }
--- CRUDAppUsingASPCorewebAPI/Models/Student.cs
+++ CRUDAppUsingASPCorewebAPI/Models/Student.cs
@@ -1,12 +1,19 @@
-﻿namespace CRUDAppUsingASPCorewebAPI.Models
+﻿using Microsoft.Build.Framework;
+
+namespace CRUDAppUsingASPCorewebAPI.Models
 {
-        public class Student
-        {
-            public int id { get; set; }
-            public string studentName { get; set; }
-            public string studentGender { get; set; }
-            public int age { get; set; }
-            public int standard { get; set; }
-            public string fatherName { get; set; }
-        }
+    public class Student
+    {
+        public int id { get; set; }
+        [Required]
+        public string studentName { get; set; }
+        [Required]
+        public string studentGender { get; set; }
+        [Required]
+        public int age { get; set; }
+        [Required]
+        public int standard { get; set; }
+        [Required]
+        public string fatherName { get; set; }
+    }
 }
